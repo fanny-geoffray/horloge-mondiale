90013e67adc2139973f4e479781e847a2085b101
fanny-geoffray <fannygeoffray69@gmail.com>
2024-03-12 09:50:18 +0100

    modif css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cityCards = document.querySelectorAll('.city-card');
+
+    function updateClock() {
+        const now = new Date();
+
+        cityCards.forEach(card => {
+            const cityName = card.getAttribute('data-city');
+            const timezone = card.getAttribute('data-timezone');
+
+            const options = { timeZone: timezone, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+            const formattedTime = new Intl.DateTimeFormat('fr-FR', options).format(now);
+
+            const timeElement = card.querySelector('.current-time');
+            if (timeElement) {
+                timeElement.textContent = `Heure actuelle : ${formattedTime}`;
+            }
+        });
+    }
+
+    // Mettre à jour l'horloge initiale
+    updateClock();
+
+    // Mettre à jour l'horloge chaque seconde
+    setInterval(updateClock, 1000);
+});
+
+
 
 
 
